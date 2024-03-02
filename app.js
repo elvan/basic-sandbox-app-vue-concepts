2a00d75b67e970364d90e4ab8f3c60fb0ccd9616
Elvan Hidayat <elvanhd@gmail.com>
2024-03-02 19:29:16 +0700

    Mount second Vue app with favorite meal feature
    - Add a second Vue app instance to manage favorite meal display
    - Mount new Vue app to a separate div element with id 'app2'
    - Update CSS to apply styling rules to both app containers
    - Demonstrate ability to manage multiple Vue instances within a page

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ const app = Vue.createApp({
 
 app.mount('#app');
 
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: 'Pizza',
+    };
+  },
+});
+
+app2.mount('#app2');
+
 // ....
 
 const data = {
