1eee4e18411b6ba14686f51542accff864fdf9a3
Elvan Hidayat <elvanhd@gmail.com>
2024-03-02 19:23:07 +0700

    Enhance app with reactive Proxy example
    - Introduce a Proxy to reactively update longMessage when message changes
    - Utilize Proxy handler to append ' World!' to message upon update
    - Demonstrate Proxy's reactivity outside of Vue's reactivity system

--- app.js
+++ app.js
@@ -16,3 +16,25 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+// ....
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello!!!!';
+
+console.log(proxy.longMessage);
